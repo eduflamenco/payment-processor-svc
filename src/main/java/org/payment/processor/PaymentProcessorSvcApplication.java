e3b850a99f12fab023fbef9b07bcba480a0707a9
edflamenco <eduardo.flamenco15@hotmail.com>
2025-07-11 13:16:47 -0600

    Initial Commit

---
+++ src/main/java/org/payment/processor/PaymentProcessorSvcApplication.java
@@ -0,0 +1,13 @@
+package org.payment.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessorSvcApplication.class, args);
+	}
+
+}
